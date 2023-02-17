6ad59198665a3e3654eeb9f49298cd4d35961c25
qlows <79415956+qlows@users.noreply.github.com>
2023-02-17 16:45:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const app = express();
 const port = 3000;
 const userData = require('./MOCK_DATA.json');
 const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
 
+const userType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        //id: { type: GraphQLInt },
+        first_name: { type: GraphQLString },
+        last_name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        gender: { type: GraphQLString },
+        salary: { type: GraphQLInt },
+    )},
+})
 
-const RootQuery = "query"
+const RootQuery = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        getAllUsers: {
+            type: new graphql.GraphQLList(userType),
+            args: { first_name: { type: GraphQLString } },
+            resolve: (parent, args) => {
+                return userData //.filter(user => user.first_name === args.first_name);
+            },
+        },
+    )},
+})
 const Mutation = "mutation"
 
 const schema = new GraphQLSchema({
