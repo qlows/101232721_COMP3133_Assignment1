d0acbc568a0282435aef45e1e3a123fe6800702b
qlows <79415956+qlows@users.noreply.github.com>
2023-02-17 17:39:45 -0500

    Added user model

---
+++ model/user.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "Enter username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Enter email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Enter password"],
+
+    },
+});
+
+module.exports = model("User", userSchema);
