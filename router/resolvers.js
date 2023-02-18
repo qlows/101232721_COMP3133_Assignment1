68a20463d62c309b7123f198a24b02f844df8bdd
qlows <79415956+qlows@users.noreply.github.com>
2023-02-17 21:00:08 -0500

    Fixed issue with fetching employee by id

--- router/resolvers.js
+++ router/resolvers.js
@@ -3,34 +3,32 @@ const Employee = require('../model/employee');
 module.exports = {
     Query: {
         // Query for fetching employee by ID
-        employee: async (_, { id }) => {
+        employee: async (_, { ID }) => {
             try {
-                const employee = await Employee.findById(id);
-
+                const employee = await Employee.findById(ID);
                 if (!employee) {
-                    throw new Error(`Employee with ID ${id} not found`);
+                    throw new Error(`Employee with ID ${ID} not found`);
                 }
                 return employee;
             } catch (error) {
                 throw new Error(`Could not find employee: ${error.message}`);
             }
         },
-
         // Query for fetching all employees
         async getEmployees() {
             try {
-              const employees = await Employee.find();
-              return employees;
+                const employees = await Employee.find();
+                return employees;
             } catch (err) {
-              return {
-                message: "Error fetching employees",
-              };
+                return {
+                    message: "Error fetching employees",
+                };
             }
-          },
-          
-
+        },
     },
+
     Mutation: {
+        // Mutation for creating a new employee
         async createEmployee(_, { input: { first_name, last_name, email, gender, salary } }) {
             const newEmployee = new Employee({
                 first_name,
@@ -46,9 +44,11 @@ module.exports = {
                 ...res._doc,
             }
         },
+        // Mutation for deleting an employee
         async deleteEmployee(_, { ID }) {
             return await Employee.findByIdAndDelete({ _id: ID });
         },
+        // Mutation for updating an employee
         updateEmployee: async (_, { id, input }) => {
             try {
                 const updatedEmployee = await Employee.findByIdAndUpdate(id, input, { new: true });
