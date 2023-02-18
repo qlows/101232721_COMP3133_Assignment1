eb3743b7f006cebd1e44daf81f2395dbb5dcb0e8
qlows <79415956+qlows@users.noreply.github.com>
2023-02-17 20:28:10 -0500

    Added query for fetching all employees

--- router/resolvers.js
+++ router/resolvers.js
@@ -2,12 +2,33 @@ const Employee = require('../model/employee');
 
 module.exports = {
     Query: {
-        async employee(_, { id }) {
-            return await Employee.findById(id);
+        // Query for fetching employee by ID
+        employee: async (_, { id }) => {
+            try {
+                const employee = await Employee.findById(id);
+
+                if (!employee) {
+                    throw new Error(`Employee with ID ${id} not found`);
+                }
+                return employee;
+            } catch (error) {
+                throw new Error(`Could not find employee: ${error.message}`);
+            }
         },
-        async getEmployees(_, { id }) {
-            return await Employee.findById(id);
-        },
+
+        // Query for fetching all employees
+        async getEmployees() {
+            try {
+              const employees = await Employee.find();
+              return employees;
+            } catch (err) {
+              return {
+                message: "Error fetching employees",
+              };
+            }
+          },
+          
+
     },
     Mutation: {
         async createEmployee(_, { input: { first_name, last_name, email, gender, salary } }) {
@@ -26,15 +47,15 @@ module.exports = {
             }
         },
         async deleteEmployee(_, { ID }) {
-            return await Employee.findByIdAndDelete({_id: ID});
+            return await Employee.findByIdAndDelete({ _id: ID });
         },
         updateEmployee: async (_, { id, input }) => {
             try {
-              const updatedEmployee = await Employee.findByIdAndUpdate(id, input, { new: true });
-              return updatedEmployee;
+                const updatedEmployee = await Employee.findByIdAndUpdate(id, input, { new: true });
+                return updatedEmployee;
             } catch (error) {
-              throw new Error(`Could not update employee: ${error.message}`);
+                throw new Error(`Could not update employee: ${error.message}`);
             }
-          },
-        }
+        },
     }
+}
