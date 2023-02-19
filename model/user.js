e38d92f6a415c9908fe9ae7afddbf9ba385319d3
qlows <79415956+qlows@users.noreply.github.com>
2023-02-19 16:30:27 -0500

    Update to user model

--- model/user.js
+++ model/user.js
@@ -1,4 +1,6 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
     username: {
@@ -15,5 +17,15 @@ const userSchema = new Schema({
         required: [true, "Enter password"],
     },
 });
+
+// Hash the password before saving the user
+/* UserSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+    }
+    next();
+}); */
 
 module.exports = model("User", userSchema, "users");
